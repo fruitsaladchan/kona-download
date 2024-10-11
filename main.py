bbfe20b08ee151c65d72fc8ed566b0944a00e27b
fruitsaladchan <ethandcardenas@gmail.com>
2024-10-11 00:33:01 -0400

    remove unnecessary stuff

--- main.py
+++ main.py
@@ -12,35 +12,6 @@ def slowprint(text, delay=1./400):
         time.sleep(delay)
     print("")
 
-def about():
-    try:
-        os.system("clear")
-        print("\033[1;32m\007\n")
-        os.system("figlet kona downloader")
-        print("")
-        slowprint("\033[1;91m -----------------------------------------------")
-        slowprint("\033[1;33m" + "         [+] Tool Name     =>\033[1;36m" + " Kona Downloader")
-        slowprint("\033[1;33m" + "         [+] Author        =>\033[1;36m" + " fruitsaladchan ")
-        slowprint("\033[1;33m" + "         [+] Latest Update =>\033[1;36m" + " 03/9/2024")
-        slowprint("\033[1;33m" + "         [+] Github        =>\033[1;36m" + " Github.com/fruitsaladchan/kona-download")
-        slowprint("\033[1;33m" + "         [+] About         =>\033[1;36m" + " Tool used for downloading images off of konachan based on tags")
-        slowprint("\033[1;91m -----------------------------------------------")
-        print(" ")
-        input("\033[1;33m [+] Press Enter To Continue [+]")
-        os.system("clear")
-        return
-
-    except KeyboardInterrupt:
-        os.system("clear")
-        return
-
-def ext():
-    slowprint("\033[1;36m ==============================================")
-    slowprint("\033[1;33m |             Thanks For Using               |")
-    slowprint("\033[1;36m ==============================================")
-    print(" ")
-    sys.exit()
-
 def download_image(url, folder):
     response = requests.get(url)
     if response.status_code == 200:
@@ -94,97 +65,45 @@ def get_images(tag, character, pages, folder_name, nsfw):
     rename_images(folder)
     print(" ")
     slowprint("All images have been Downloaded.")
-
-def konachan_downloader():
-    while True:
-        try:
-            os.system("figlet Downloader")
-            slowprint("\033[1;36m ==============================================")
-            print(" ")
-            tag = input("Enter tags (eg long_hair, skirt, original, touhou. etc): ").strip()
-            character = input("Enter characters (eg hatsune_miku, kagamine_rin, yakumo_yukari etc): ").strip()
-
-            pages_input = input("Enter pages (eg. 1 3 5 or 1-5 | default is 1 page): ").strip()
-            if not pages_input:
-                pages = [1]  # Default to page 1 if no input
-            else:
-                pages = parse_pages(pages_input)
-
-            folder_name = input("Enter folder name: ").strip()
-            if not folder_name:
-                folder_name = "images"
-            
-            while True:
-                nsfw_input = input("Do you want NSFW images? (yes/no leave blank for NSFW): ").strip().lower()
-                if nsfw_input in ['yes', 'no', '']:
-                    nsfw = nsfw_input in ['yes', '']
-                    break
-                else:
-                    print("\033[1;91mInvalid input! Please enter 'yes', 'no', or leave blank for NSFW.\033[0m")
-
-            print(" ")
-            slowprint("\033[1;36m ==============================================")
-            folder_name = os.path.join(os.getcwd(), folder_name)
-            
-            get_images(tag, character, pages, folder_name, nsfw)
-            print(" ")
-            slowprint("\033[1;36m ==============================================")
-            print(" ")
-            input("\033[1;36mPress Enter to return ...")
-            os.system("clear")
-            break
-
-        except KeyboardInterrupt:
-            os.system("clear")
-            return
+    slowprint("Cleaning up...")
+    print(" ")
+    slowprint("\033[1;36m ==============================================")
+    sys.exit()
 
 def main():
-    while True:
-        try:
-            os.system("clear")
-            print("\033[1;36m")
-            os.system("figlet Kona Downloader")
-            slowprint("\033[1;36m ==============================================")
-            slowprint(" ")
+    try:
+        os.system("figlet Kona Downloader")
+        slowprint("\033[1;36m ==============================================")
+        print(" ")
+        tag = input("Enter tags (eg long_hair, skirt, original, touhou. etc): ").strip()
+        character = input("Enter characters (eg hatsune_miku, kagamine_rin, yakumo_yukari etc): ").strip()
 
-            menu_options = [
-                "\033[1;33m [ 1 ]\033[1;91m Konachan Image Downloader",
-                "\033[1;33m [ 2 ]\033[1;91m About This Tool",
-                "\033[1;33m [ 3 ]\033[1;91m Exit",
-            ]
+        pages_input = input("Enter pages (eg. 1 3 5 or 1-5 | default is 1 page): ").strip()
+        if not pages_input:
+            pages = [1]  # Default to page 1 if no input
+        else:
+            pages = parse_pages(pages_input)
 
-            for option in menu_options:
-                slowprint(option)
-            
-            print("     ")
-
-            option = input("\033[1;36m [+] >> \033[1;32m")
-            
-            if option == "1":
-                os.system("clear")
-                konachan_downloader()
-
-            elif option == "2":
-                os.system("clear")
-                about()
-
-            elif option == "3":
-                os.system("clear")
-                ext()
-
+        folder_name = input("Enter folder name: ").strip()
+        if not folder_name:
+            folder_name = "images"
+        
+        while True:
+            nsfw_input = input("Do you want NSFW images? (yes/no leave blank for NSFW): ").strip().lower()
+            if nsfw_input in ['yes', 'no', '']:
+                nsfw = nsfw_input in ['yes', '']
+                break
             else:
-                print(" ")
-                slowprint("\033[1;91m Enter Correct Number!!!")
-                time.sleep(1)
-                os.system("clear")
+                print("\033[1;91mInvalid input! Please enter 'yes', 'no', or leave blank for NSFW.\033[0m")
 
-        except KeyboardInterrupt:
-            os.system("clear")
-            slowprint("\033[1;36m ==============================================")
-            slowprint("\033[1;33m |             Thanks for using               |")
-            slowprint("\033[1;36m ==============================================")
-            print(" ")
-            sys.exit()
+        print(" ")
+        slowprint("\033[1;36m ==============================================")
+        folder_name = os.path.join(os.getcwd(), folder_name)
+        
+        get_images(tag, character, pages, folder_name, nsfw)
+
+    except KeyboardInterrupt:
+        return
 
 if __name__ == "__main__":
     main()
